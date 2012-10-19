77520f03bc38e318124ce3c0e256afabda4d9727
a@mac-mini-2.local <mikeeto@hotmail.com>
2012-10-19 01:43:53 -0600

    index.html update rbr.txt added

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,10 +1,15 @@
 $(".box1").hide();
 $(".box2").hide();
+$(".box3").hide();
 $(".bbox1").click(function(){
     $(".box1").fadeToggle('slow');
     return false;
 });
 $(".bbox2").click(function(){
     $(".box2").fadeToggle('slow');
+    return false;
+});
+$(".bbox3").click(function(){
+    $(".box3").fadeToggle('slow');
     return false;
 });
